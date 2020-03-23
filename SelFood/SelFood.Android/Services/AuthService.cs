8d5443f85d6f8ef043b378fccc39b385af34c393
Bryant Steven Rincon Valderrama <Stevenrincon_01@hotmail.com>
2020-03-23 18:09:18 -0500

    Add project files.

---
+++ SelFood/SelFood.Android/Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using SelFood.Services.Interfaces;
+
+namespace SelFood.Droid.Services
+{
+    public class AuthService : IAuthService
+    {
+        public async Task<string> SignIn(string email, string password)
+        {
+            try
+            {
+                var user = await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = await user.User.GetIdTokenAsync(false);
+                return token.Token;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<string> SignUp(string email, string password)
+        {
+            try
+            {
+                var user = await FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+                var token = await user.User.GetIdTokenAsync(false);
+                return token.Token;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+    }
+}
---
+++ SelFood/SelFood/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SelFood.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> SignUp(string email, string password);
+        Task<string> SignIn(string email, string password);
+    }
+}

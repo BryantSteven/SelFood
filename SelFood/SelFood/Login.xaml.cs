8dee5a4880da7fd6a34020139a1ee1bc008a3a65
Bryant Steven Rincon Valderrama <Stevenrincon_01@hotmail.com>
2020-03-24 14:00:43 -0500

    Pantallas login register

--- SelFood/SelFood/Login.xaml.cs
+++ SelFood/SelFood/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Paket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,15 +24,15 @@ namespace SelFood
 
         async void LoginBtn_Clicked(System.Object sender, System.EventArgs e)
         {
-           /*
-            var authserve = new authservice();
-            var token = await authservice.login(email.text, password.text);
-            if (!string.isnullorempty(token)) {
-                navigation.pushmodalasync(new homepage());
-                return;
-            }
            
-            System.console.writeline("no se pudo iniciar sesión");*/
+            //var authserve = new AuthService();
+            //var token = await Authservice.login(email.text, password.text);
+            //if (!string.isnullorempty(token)) {
+            //    navigation.pushmodalasync(new homepage());
+            //    return;
+            //}
+           
+            //System.console.writeline("no se pudo iniciar sesión");
             System.Console.WriteLine(Email.Text);
             System.Console.WriteLine(Password.Text);
         }

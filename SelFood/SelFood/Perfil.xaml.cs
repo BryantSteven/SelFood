b966dd5374c22adc43f426deeadc7160829a0478
luis camacho <luiscamacho20000@outlook.com>
2020-04-28 11:24:30 -0500

    Interfaz Perfil

---
+++ SelFood/SelFood/Perfil.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SelFood
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Perfil : ContentPage
+    {
+        public Perfil()
+        {
+            InitializeComponent();
+            string s = "d";
+           // Username.Text = s; 
+        }
+        async void GoBackBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+            
+        }
+        
+        async void Btn_Image_Cliked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}

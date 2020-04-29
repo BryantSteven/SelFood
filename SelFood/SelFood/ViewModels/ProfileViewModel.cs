c858f0530e8876487d329b7aab8d615240165a45
Bryant Steven <StevenRincon_01@hotmail.com>
2020-04-28 19:07:05 -0500

    xd

---
+++ SelFood/SelFood/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using firebasesample.Services.FirebaseDB;
+using System;
+using Xamarin.Forms;
+
+namespace SelFood.ViewModels
+{
+    class ProfileViewModel
+    {
+        private IFirebaseDBService _firebaseDatabaseService;
+
+        public ProfileViewModel() {
+            _firebaseDatabaseService = DependencyService.Get<IFirebaseDBService>();
+            _firebaseDatabaseService.Connect();
+            _firebaseDatabaseService.GetMessage();
+            MessagingCenter.Subscribe<String, String>(this, _firebaseDatabaseService.GetMessageKey(), (sender, args) =>
+            {
+                var message = (args);
+
+            });
+        }
+    }
+}
